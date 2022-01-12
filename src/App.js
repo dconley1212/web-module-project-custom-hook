a7e61d98eb69419c5fb4dc4ad53ebc63379e949b
David Conley <dconley1989@gmail.com>
2022-01-12 15:27:51 -0700

    created state logical hook and file and folder

--- src/App.js
+++ src/App.js
@@ -6,18 +6,25 @@ import Navbar from "./components/Navbar";
 
 import "./style.css";
 
+const useDarkMode = () => {
+  const [booleanValue, setBooleanValue] = useState(false);
+
+  return [booleanValue, setBooleanValue];
+};
+
 const App = () => {
   const [coinData, setCoinData] = useState([]);
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useDarkMode(false);
 
   useEffect(() => {
     axios
       .get(
         "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
       )
-      .then(res => setCoinData(res.data))
-      .catch(err => console.log(err));
+      .then((res) => setCoinData(res.data))
+      .catch((err) => console.log(err));
   }, []);
+
   return (
     <div className={darkMode ? "dark-mode App" : "App"}>
       <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
